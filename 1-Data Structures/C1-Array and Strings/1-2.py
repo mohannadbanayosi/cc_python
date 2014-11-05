81145ea12c0ebca9171bd2ea31dd01eba495eecd
Mohannad Banayosi <m.banayosi@gmail.com>
2014-11-05 02:28:56 +0200

    Enhanced 1-2

--- 1-Data Structures/C1-Array and Strings/1-2.py
+++ 1-Data Structures/C1-Array and Strings/1-2.py
@@ -1,8 +1,10 @@
 def reverse_string(string):
-	reversed_string = []
 	string_length = len(string)
-	for i in range(0, string_length):
-		reversed_string.append(string[string_length-i-1])
-	return reversed_string
+	for i in range(0, string_length/2):
+		temp = string[i]
+		string[i] = string[string_length-i-1]
+		string[string_length-i-1] = temp
+	return string
 
-print reverse_string("python")
+print reverse_string(list("python"))
+print "".join(reverse_string(list("python")))
